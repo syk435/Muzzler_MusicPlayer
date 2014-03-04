97dbc250428ad9a10f300340cff80d7105404c2f
Salem Karani <salemkarani@gmail.com>
2014-03-04 13:54:27 -0600

    sample gzip compressed PUT js

---
+++ sample_put_request_compression.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">var sr_gzipEnabled = false;</script> 
+<script type="text/javascript" src="http://d2ft4b0ve1aur1.cloudfront.net/js-050/sr.gzipcheck.js.jgz"></script> 
+
+<noscript> 
+  <link type="text/css" rel="stylesheet" href="http://d2ft4b0ve1aur1.cloudfront.net/css-050/sr-br-min.css">
+</noscript> 
+<script type="text/javascript"> 
+(function () {
+    var sr_css_file = 'http://d2ft4b0ve1aur1.cloudfront.net/css-050/sr-br-min.css';
+    if (sr_gzipEnabled) {
+      sr_css_file = 'http://d2ft4b0ve1aur1.cloudfront.net/css-050/sr-br-min.css.gz';
+    }
+
+    var head = document.getElementsByTagName("head")[0];
+    if (head) {
+        var scriptStyles = document.createElement("link");
+        scriptStyles.rel = "stylesheet";
+        scriptStyles.type = "text/css";
+        scriptStyles.href = sr_css_file;
+        head.appendChild(scriptStyles);
+        //alert('adding css to header:'+sr_css_file);
+     }
+}());
+</script> 
